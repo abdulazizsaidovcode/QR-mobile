9dcaa93723042445740ca977ab20fb939d6b1417
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-09-30 18:38:17 +0500

    sa

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import Feather from '@expo/vector-icons/Feather'
+
+const Navbar = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.greetingContainer}>
+        <Avatar
+          rounded
+          size="medium"
+          overlayContainerStyle={{ backgroundColor: 'lightgray' }}
+          icon={{ name: 'user', type: 'font-awesome', color: 'white' }}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.greetingText}>Hi, John!</Text>
+          <Text style={styles.subText}>Good Morning</Text>
+        </View>
+      </View>
+      <Feather name="bell" size={24} color="black" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  greetingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textContainer: {
+    marginLeft: 10,
+  },
+  greetingText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 14,
+    color: 'gray',
+  },
+});
+
+export default Navbar;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,26 +3,28 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
+  bodyColor: "#f9f9f9",
+
   light: {
-    text: '#11181C',
-    primary: '#ff5e2c',
-    background: '#fff',
+    text: "#11181C",
+    primary: "#ff5e2c",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    primary: '#ff5e2c',
-    background: '#151718',
+    text: "#ECEDEE",
+    primary: "#ff5e2c",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
 };
