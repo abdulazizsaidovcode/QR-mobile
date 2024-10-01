3992e874d41e8cfba49942b404c38e0b957e581d
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-10-01 17:13:43 +0500

    d

--- helpers/url.tsx
+++ helpers/url.tsx
@@ -3,3 +3,4 @@ const apiLink = process.env.REACT_APP_API_URL
 
 export const staisticUrl = `${apiLink}statistics/seller` 
 export const loginUrl = `${apiLink}api/user/login` 
+export const createPayment = `${apiLink}payment/create`
