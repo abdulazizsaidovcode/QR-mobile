14dc6e720dc211b74f49bd9dde15ba6257173c8b
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-10-01 14:07:12 +0500

    4

---
+++ helpers/api - functions/global-response.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const globalresponse = () => {
+  return (
+    <View>
+      <Text>globalresponse</Text>
+    </View>
+  )
+}
+
+export default globalresponse
+
+const styles = StyleSheet.create({})
